b87dfc0b73b86555dbfae7baf0491c8ce34356d0
iammilaad <miladheydari.work@gmail.com>
2025-02-24 11:11:37 +0330

    move pwa metatags from component to html

--- src/components/meta-tags.tsx
+++ src/components/meta-tags.tsx
@@ -22,12 +22,6 @@ export default function MetaTags({
       <meta property="og:image" content={thumbnail} />
       <meta property="og:url" content={import.meta.env.VITE_SITE_URL} />
       <meta property="og:type" content="website" />
-      <link rel="icon" type="image/png" href="/icons/favicon-96x96.png" sizes="96x96" />
-      <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
-      <link rel="shortcut icon" href="/icons/favicon.ico" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
-      <meta name="apple-mobile-web-app-title" content="StarPlus" />
-      <link rel="manifest" href="/site.webmanifest" />
     </Helmet>
   );
 }
